6d49ca524854bd068b454885f0f708dac8eab235
Salis <m.salis@go-jek.com>
2017-04-04 03:09:44 +0700

    add mvvm display screen

--- app/src/main/java/com/bloonerd/androidukdw/mvvm/MVVMDisplayActivity.java
+++ app/src/main/java/com/bloonerd/androidukdw/mvvm/MVVMDisplayActivity.java
@@ -1,13 +1,80 @@
 package com.bloonerd.androidukdw.mvvm;
 
+import android.content.Context;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bloonerd.androidukdw.R;
+import com.bloonerd.androidukdw.UserPreference;
+import com.bloonerd.androidukdw.model.User;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MVVMDisplayActivity extends AppCompatActivity {
+
+    private static final String TAG = "CommonDisplayActivity";
+    private ImageView profile;
+    private TextView name;
+    private ImageView imgEmail;
+    private TextView textEmail;
+    private ImageView imgGender;
+    private TextView textGender;
+    private TextView textDob;
+    private Button getUser;
+    private UserPreference userPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvvmdisplay);
+        userPreference = UserPreference.getInstance(this);
+        initView();
+        setUser();
+    }
+
+    private void initView() {
+        profile = (ImageView) findViewById(R.id.img_profile);
+        name = (TextView) findViewById(R.id.text_name);
+        imgEmail = (ImageView) findViewById(R.id.img_email);
+        textEmail = (TextView)  findViewById(R.id.text_email);
+        imgGender = (ImageView) findViewById(R.id.img_gender);
+        textGender = (TextView) findViewById(R.id.text_gender);
+        textDob = (TextView) findViewById(R.id.text_dob);
+        getUser = (Button) findViewById(R.id.btn_get_user);
+    }
+
+    private void setUser() {
+        final Context context = this;
+        getUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User user = userPreference.getUser();
+                MVVMDisplayViewModel viewModel = new MVVMDisplayViewModel(context, user);
+
+                profile.setImageDrawable(viewModel.getProfile());
+                name.setText(viewModel.getName());
+
+                imgEmail.setImageDrawable(viewModel.getEmailIcon());
+                textEmail.setText(viewModel.getEmail());
+
+                imgGender.setImageDrawable(viewModel.getGenderIcon());
+                textGender.setText(user.gender);
+
+                textDob.setText(user.dob);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/bloonerd/androidukdw/mvvm/MVVMDisplayViewModel.java
+++ app/src/main/java/com/bloonerd/androidukdw/mvvm/MVVMDisplayViewModel.java
@@ -1,4 +1,81 @@
 package com.bloonerd.androidukdw.mvvm;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.Log;
+
+import com.bloonerd.androidukdw.R;
+import com.bloonerd.androidukdw.model.User;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class MVVMDisplayViewModel {
+
+    private static final String TAG = "MVVMDisplayViewModel";
+    private final Context context;
+    final private User user;
+
+    public MVVMDisplayViewModel(Context context, User user) {
+        this.context = context;
+        this.user = user;
+    }
+
+    public RoundedBitmapDrawable getProfile() {
+        RoundedBitmapDrawable imgRound = RoundedBitmapDrawableFactory.create(context.getResources(), user.image);
+        imgRound.setCircular(true);
+        return imgRound;
+    }
+
+    public String getName() {
+        Date date = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yy", Locale.getDefault());
+        try {
+            date = dateFormat.parse(user.dob);
+        } catch (ParseException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        if (date != null) {
+            long diff = System.currentTimeMillis() - date.getTime();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(diff);
+            double year = calendar.get(Calendar.YEAR) - 1970;
+
+            if (year <= 17) {
+                return "Dek " + user.firstName + " " + user.lastName;
+            } else {
+                return user.firstName + " " + user.lastName;
+            }
+        } else {
+            return "";
+        }
+    }
+
+    public Drawable getEmailIcon() {
+        if (user.email.contains("gmail"))
+            return ContextCompat.getDrawable(context, R.drawable.ic_gmail);
+        else if (user.email.contains("yahoo"))
+            return ContextCompat.getDrawable(context, R.drawable.ic_yahoo);
+        else
+            return ContextCompat.getDrawable(context, R.drawable.ic_hotmail);
+    }
+
+    public String getEmail() {
+        return user.email;
+    }
+
+    public Drawable getGenderIcon() {
+        if (user.gender.equalsIgnoreCase("female"))
+            return ContextCompat.getDrawable(context, R.drawable.ic_women);
+        else
+            return ContextCompat.getDrawable(context, R.drawable.ic_men);
+    }
 }
