fe3f458f10b0e3b889a5240c08d85c45621c65e7
Salis <m.salis@go-jek.com>
2017-04-04 13:45:33 +0700

    add mockito library

---
+++ app/src/test/java/com/bloonerd/androidukdw/mvp/MVPPresenterTest.java
@@ -0,0 +1,14 @@
+package com.bloonerd.androidukdw.mvp;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class MVPPresenterTest {
+
+
+
+}
