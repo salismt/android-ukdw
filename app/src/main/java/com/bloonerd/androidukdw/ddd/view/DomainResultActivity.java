35238c56c8f98e1be4629d21029956235d46046c
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2017-04-04 09:29:50 +0700

    [Budi] Implement Domain Driven Design UI Input

---
+++ app/src/main/java/com/bloonerd/androidukdw/ddd/view/DomainResultActivity.java
@@ -0,0 +1,33 @@
+package com.bloonerd.androidukdw.ddd.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.bloonerd.androidukdw.R;
+import com.bloonerd.androidukdw.ddd.domain.contract.UserService;
+import com.bloonerd.androidukdw.ddd.domain.model.User;
+import com.bloonerd.androidukdw.ddd.services.UserServices;
+
+public class DomainResultActivity extends AppCompatActivity {
+
+    UserService userService;
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domain_result);
+        userService = new UserServices();
+
+        // Load data yang di bawa dari activity sebelumnya melalui android bundle object
+        final User user = userService.load(getIntent().getBundleExtra("USER_BUNDLE"));
+        final String loadUsername = user.getUsername();
+        final String loadPassword = user.getPassword();
+
+        final TextView viewUsername = (TextView) findViewById(R.id.tv_username);
+        final TextView viewPassword = (TextView) findViewById(R.id.tv_password);
+        viewUsername.setText(loadUsername);
+        viewPassword.setText(loadPassword);
+    }
+}
